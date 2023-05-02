98702a49ac6f6525b592841144a00f35e46e44ee
ilya <ilia.lobow@gmail.com>
2023-05-03 00:20:27 +0300

    рекурсивная функция

--- src/main/java/Smart/IntegerDigitsCount.java
+++ src/main/java/Smart/IntegerDigitsCount.java
@@ -4,5 +4,23 @@ public class IntegerDigitsCount {
 
     public static int count(int n) {
         // your recursive solution in here
+        char[] chars = String.valueOf(n).toCharArray();
+        return chars.length;
     }
+
+
+    public static int countOne(int n) {
+        return Math.abs(n) < 10 ? 1 : 1 + countOne(n/10);
+    }
+
+    public static int countTwo(int t) {
+        if (Math.abs(t) < 10)
+            return 1;
+        else return 1 + countTwo(t/10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(IntegerDigitsCount.countOne(2000));
+    }
+
 }
--- src/test/java/TestTraining.java
+++ src/test/java/TestTraining.java
@@ -1,16 +1,21 @@
+import Smart.IntegerDigitsCount;
 import Smart.PinValidate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.regex.Pattern;
 
 public class TestTraining {
     PinValidate pinValidate;
+    IntegerDigitsCount integerDigitsCount;
 
     @Test
     public void pinValidateTest(){
         Assertions.assertTrue(PinValidate.validate("2334"));
+    }
+
+    @Test
+    public void countTest() {
+        Assertions.assertEquals(5,IntegerDigitsCount.count(2345));
 
     }
 }
